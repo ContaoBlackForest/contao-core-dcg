14dfb45e1b70d325d468034c63fe6179cbae7780
Sven Baumann <baumann.sv@gmail.com>
2017-07-18 23:12:54 +0200

    change to contao bundle

---
+++ src/CbContaoCoreDcgBundle.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * FRAMEWORK
+ *
+ * Copyright (C) FRAMEWORK
+ *
+ * @package   contao-core-dcg
+ * @file      CbContaoCoreDcgBundle.php
+ * @author    Sven Baumann <baumann.sv@gmail.com>
+ * @author    Dominik Tomasi <dominik.tomasi@gmail.com>
+ * @license   GNU/LGPL
+ * @copyright Copyright 2017 owner
+ */
+
+
+namespace ContaoBlackForest\Contao\Core\DcGeneral;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class CbContaoCoreDcgBundle extends Bundle
+{
+
+}
